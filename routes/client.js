bca9584230a081db1e334456410467832f2d407c
Ankitluhar <ankitluhar5121@gmail.com>
2024-05-03 04:35:08 +0530

    first commit

---
+++ routes/client.js
@@ -0,0 +1,16 @@
+import express from "express";
+import {
+  getProducts,
+  getCustomers,
+  getTransactions,
+  getGeography,
+} from "../controllers/client.js";
+
+const router = express.Router();
+
+router.get("/product", getProducts);
+router.get("/customers", getCustomers);
+router.get("/transactions", getTransactions);
+router.get("/geography", getGeography);
+
+export default router;
